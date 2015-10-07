94c0c163e2441a244f6c7cb15d47386f0a0f49e8
Yu Shen <yubrshen@gmail.com>
2015-10-07 17:35:40 +0800

    Initial implementation working for basic functionality.

---
+++ multiply.js
@@ -0,0 +1,59 @@
+var range = 10;
+var left = 0;
+var right = 0;
+var expected = 0;
+
+var upperPast = range * range;
+var lowerLast = -1;
+
+function drill() {
+    left = Math.floor(Math.random() * range);
+    right = Math.floor(Math.random() * range);
+    expected = left * right;
+}
+
+function present() {
+    drill();
+    $("#left").html(left);
+    $("#right").html(right);
+    $("#encouragement").html("Be courageous, just keep trying...");
+    // clear inputs
+    $(".input").val('');
+    }
+
+$("#check").click(function () {
+    var equal = parseInt($("#equal").val());
+    var upper = parseInt($("#upper").val());
+    var lower = parseInt($("#lower").val());
+    
+    if (equal == expected) {
+        // $("#encouragement").html("CORRECT!!! Please try more...");
+        if (confirm("CORRECT!!! Would you try more?")) {
+            present();
+            return;
+        } // to improve, to have a button to confirm if continue through Javascript
+    } else if (expected < lower && upper < expected) {
+        $("#encouragement").html("Sorry, none of your estimate is correct, please reflect and try again.");
+        return;
+    }
+    $("#encouragement").html("");
+    if (upper < upperPast && expected <= upper) {
+        $("#encouragement").html("Better estimate of the upper bound!");
+        upperPast = upper;
+    } else {
+        $("#encouragement").html("Try better upper bound!");
+    }
+    if (lowerLast < lower && lower <= expected) {
+        $("#encouragement").append("<p>Better estimate of the lower bound!</p>");
+        lowerLast = lower;
+    } else {
+        $("#encouragement").append("<p>Try better lower bound!</p>");
+    }
+    $("#encouragement").append("<p>Based on the estimate, can you come up better solution?</p>");
+    // give hint here
+});
+
+present();
+
+
+
